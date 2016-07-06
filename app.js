932f70a68a2c8155048182e71de1738ec6f198dc
Andrey <sokolik_85@list.ru>
2016-07-06 11:07:07 +0000

    added user model and authentication

--- app.js
+++ app.js
@@ -2,17 +2,39 @@ var express = require("express"),
     app = express(), 
     bodyParser = require("body-parser"), 
     mongoose = require("mongoose"),
+    passport = require('passport'),
+    LocalStratagy = require("passport-local"),
     //User = require("./models/user"),
     Comment = require("./models/comment"),
+    User   = require("./models/user"),
     seedDB = require("./seeds"),
     Campground = require("./models/campground");
 
-seedDB();    
+  
 mongoose.connect('mongodb://' + process.env.IP + '/yelp_camp');
  
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
+
+seedDB();  
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: 'anything you want!',
+    resave: false,
+    saveUninitialized: false
+    
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStratagy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 app.get('/', function(req, res){
     res.render('landing');
@@ -107,6 +129,25 @@ app.post('/campgrounds/:id/comments', function(req, res){
     // create new comment
     // connect new comment to campground
     // redirect
+});
+
+
+// AUTH ROUTS
+app.get('/register', function(req, res) {
+    res.render('register');
+});
+
+app.post('/register', function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        } 
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('/campgrounds');
+        });
+    })
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
