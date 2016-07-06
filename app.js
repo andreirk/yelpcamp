ac5607b66a0e4a0cf08cd9c006b8f82a435e2187
Andrey <sokolik_85@list.ru>
2016-07-06 11:16:48 +0000

    added user login logic and login page

--- app.js
+++ app.js
@@ -147,7 +147,19 @@ app.post('/register', function(req, res){
         passport.authenticate('local')(req, res, function(){
             res.redirect('/campgrounds');
         });
-    })
+    });
+});
+
+// show login form
+app.get('/login', function(req, res) {
+   res.render('login'); 
+});
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/campgrounds',
+    failureRedirect: '/login'
+}), function(req, res) {
+     
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
